438efceae5020e8a9f2f0f189b244013ebca9ad3
rishabhs12 <79623723+rishabhs12@users.noreply.github.com>
2022-03-31 22:35:05 +0530

    commit

---
+++ 3_Implementation/src/advance.h
@@ -0,0 +1,10 @@
+float sqroot(float a);
+float power(float a, float b);
+float expo(float a);
+float logarithm(float a);
+float sin_res(float a);
+float cos_res(float a);
+float tan_res(float a);
+float cot_res(float a);
+float sec_res(float a);
+float cosec_res(float a);
